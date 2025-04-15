221e23089dd5f8b2a42977e07888b7fcae93860c
Norisuke1028 <138739405+Norisuke1028@users.noreply.github.com>
2025-04-15 11:02:52 +0900

    AppMainの追加
    AppMainの追加

---
+++ TGC_Game/TGC_Game/AppMain.cpp
@@ -0,0 +1,11 @@
+#include"DxLib.h"
+
+#define FRAMERATE 60.0		//フレームレート（1秒間の描画回数）
+#define SCREEN_HEIGHT 720	// 画面の高さ
+#define SCREEN_WIDTH 1280	// 画面の幅
+#define REFRESHRATE 32		// リフレッシュレート
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine, int nCmdShow)
+{
+}
