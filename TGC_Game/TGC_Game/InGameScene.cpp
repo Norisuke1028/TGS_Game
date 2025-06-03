3b4796ae5b4c32f6041817c4373fb3b5c91483fa
Norisuke1028 <138739405+Norisuke1028@users.noreply.github.com>
2025-06-03 12:01:01 +0900

    使わない画像削除
    あ

--- TGC_Game/TGC_Game/InGameScene.cpp
+++ TGC_Game/TGC_Game/InGameScene.cpp
@@ -39,7 +39,7 @@ void InGameScene::Initialize()
 	start_image = LoadGraph("Resource/image/start.png");  //スタート画像
 	LoadDivGraph("Resource/image/guzai04.png", 4, 4, 1, 200, 170, select_burger_image);  //選んだ具材画像
 	LoadDivGraph("Resource/image/burger_model.png", 6, 6, 1, 266.6, 140, burger_model);  //お題バーガー画像
-	LoadDivGraph("Resource/image/Num.png", 10, 10, 1, 49, 80, gb_number_image);  //ゲーム内で使用するナンバー画像(黒)
+	LoadDivGraph("Resource/image/number.png", 10, 10, 1, 49, 80, gb_number_image);  //ゲーム内で使用するナンバー画像(黒)
 	LoadDivGraph("Resource/image/red_number.png", 10, 10, 1, 50, 80, gr_number_image);  //ゲーム内で使用するナンバー画像(赤)
 	cursol_se = LoadSoundMem("Resource/sounds/cursol.mp3");  //カーソル音
 	correct_se = LoadSoundMem("Resource/sounds/correct.mp3");  //正解音
